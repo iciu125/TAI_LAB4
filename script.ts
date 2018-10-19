de63ed90196ff087001d1c5a1af611b371714278
Witek <>
2018-10-19 15:15:27 +0200

    zad4

--- script.ts
+++ script.ts
@@ -1,26 +1,19 @@
-var result = ' x   ';
-
-function buff(val){
-    var buff = '';
-    var pad = 4 - val;
-    while( pad-- > 0 )
-        buff += ' ';
-    return buff;
-}
-
+var result = 'x ';
 for (var i = 0; i < 11; i++) {
 
     for (var j = 0; j < 11; j++) {
 
         if(i == 0 && j > 0){
-            result += '[' + j + ']' + buff((j+'').length+2);
+            result += '[' + j + ']';
         }
         else if(j == 0 && i>0){
-            result += '[' + i + ']';
+            result += '[' + i + '] ';
         }
         else if(i>0 && j>0){
-            result += buff((i*j+'').length ) + i*j;
+            result += (i*j) + ' ';
         }
     }
     result += '\n'
 }
+
+console.log(result);
